44efefc77380316b5d2023c9af84502a63bfda16
Vanessa Bou <rvbouu@gmail.com>
2024-05-27 16:44:21 -0500

    made projects array and images

---
+++ src/assets/projects.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: 'TekTok',
+    description: 'A social media site for people interested in tech to come together.',
+    image: './assets/project_imgs/tektok.jpg',
+    liveLink: 'https://tektok.onrender.com/',
+    github: 'https://github.com/rvbouu/tektok'
+  },
+  {
+    title: 'Note Taker',
+    description: 'A note taker application made with Express.',
+    image: './assets/project_imgs/note-taker.png',
+    liveLink: 'https://note-taker-s49p.onrender.com/',
+    github: 'https://github.com/rvbouu/note-taker'
+  },
+  {
+    title: 'Hoop Stats',
+    description: 'An all-in-one place for NBA fans to keep up with the league and their favorite teams.',
+    image: './assets/project_imgs/hoop-stats-logo.png',
+    liveLink: 'https://rvbouu.github.io/hoop-stats/',
+    github: 'https://github.com/rvbouu/hoop-stats'
+  },
+  {
+    title: 'Kanban Task Board',
+    description: 'A kanban board to help manage all your tasks.',
+    image: './assets/project_imgs/task-board.png',
+    liveLink: 'https://rvbouu.github.io/task-board/',
+    github: 'https://github.com/rvbouu/task-board'
+  },
+  {
+    title: 'Personal Blog',
+    description: 'A blog site where users can post and view blogs.',
+    image: './assets/project_imgs/personal-blog.png',
+    liveLink: 'https://rvbouu.github.io/personal-blog/',
+    github: 'https://github.com/rvbouu/personal-blog'
+  },
+  {
+    title: 'My First Website',
+    description: 'The start of my web development journey! The first website I ever designed and deployed to the internet.',
+    image: './assets/project_imgs/first-website.png',
+    liveLink: 'https://rvbouu.github.io/portfolio/',
+    github: 'https://github.com/rvbouu/portfolio'
+  },
+]
