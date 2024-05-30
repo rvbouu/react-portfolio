066a3b652048a3936cc432b15275e97378f6df84
Vanessa Bou <rvbouu@gmail.com>
2024-05-30 12:19:12 -0500

    added comments to portfolio files

--- src/components/pages/portfolio/Portfolio.jsx
+++ src/components/pages/portfolio/Portfolio.jsx
@@ -1,15 +1,19 @@
-import projects from '../../../assets/projects.json'
+// imports
+import projects from '../../../assets/projects.json' // json file containing project details
 import Project from './Project'
 import './Portfolio.css'
 
+// exports
 export default function Portfolio() {
-
   return (
     <section className='portfolio'>
       <h2 className='portfolio-title'>Portfolio</h2>
+
+      {/* Where project cards will get dynamically rendered */}
       <div className='projects'>
+        {/* mapping over projects array */}
         {projects.map((project, i) => (
-          
+          // Project component that will render multiple times
           <Project project={project} key={i} />
 
         ))}
--- src/components/pages/portfolio/Project.jsx
+++ src/components/pages/portfolio/Project.jsx
@@ -1,14 +1,22 @@
+// exports
 export default function Project({project}){
   return (
+    // one project card template that sets project img as background
     <div className='projectcard' style={{ backgroundImage: `url(${project.image})`, backgroundRepeat: 'no-repeat' }}>
       <div className='links'>
+
+        {/* Live link to project */}
         <a href={project.liveLink} target='_blank'>
           <h3 className='title'>{project.title}</h3>
         </a>
+
+        {/* GitHub Repository link for project */}
         <a href={project.github} target='_blank'>
           <img src='/social_imgs/github.png' alt='github logo' id='github-logo' />
         </a>
       </div>
+
+      {/* short description for project */}
       <p className='desc'>{project.description}</p>
     </div>
   )
