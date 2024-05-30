fb61a4e904c8eeb323660ce70acdb13ee58e2dac
Vanessa Bou <rvbouu@gmail.com>
2024-05-30 12:14:00 -0500

    added comments to contact files

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,11 +1,17 @@
+// imports
 import { useState } from 'react';
-import Popup from 'reactjs-popup';
+import Popup from 'reactjs-popup'; // popup library for contact icon
 import './Contact.css';
 
+// exports
 export default function Contact() {
+  // allows form to be manipulated
   const [formData, setFormData] = useState({ name: '', email: '', msg: '' })
+
+  // allows errMsg to get rendered
   const [errMsg, setErrMsg] = useState('');
 
+  // checks form inputs: if form field is left empty, displays an errMsg to the user
   const validateForm = (e) => {
     console.log(e);
     const { name, value } = e.target;
@@ -24,6 +30,8 @@ export default function Contact() {
         setFormData({ ...formData, [name]: '' });
         return setErrMsg('REQUIRED: Please Enter an Email Address.')
       }
+
+      // checks for a valid email
       const validEmail = /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/;
       console.log(!validEmail.test(value))
       if (!validEmail.test(value)) {
@@ -47,21 +55,30 @@ export default function Contact() {
   return (
     <section className='contact'>
       <h2 className='contact-title'>Contact Me</h2>
+
+      {/* Testing submission handling through netlify */}
+      {/* onBlur used for when user clicks out of field and leaves it empty, the errMsg will display */}
       <form className='form' netlify >
 
+        {/* Name input field */}
         <label className='label' htmlFor="name">Name:</label>
         <input className='input' name="name" type="text" defaultValue={formData.name} placeholder='Enter Your Name' onBlur={validateForm} required />
 
+        {/* Email input field */}
         <label className='label' htmlFor="email">Email:</label>
         <input className='input' name="email" type="email" defaultValue={formData.email} placeholder='Enter Your Email Address' onBlur={validateForm} required />
 
+        {/* Message input field */}
         <label className='label' htmlFor="msg">Message:</label>
         <textarea className='input' name="msg" id='msg' type="text" defaultValue={formData.msg} placeholder='Enter A Message' onBlur={validateForm} required></textarea>
 
         <button id="submit" type='submit' >Submit</button>
 
+        {/* errMsg */}
         <div className='errMsg'>{errMsg}</div>
       </form>
+
+      {/* Popup for contact icon to display direct contact info */}
       <Popup
         trigger={
           <img src='/social_imgs/contact_img.png' alt='contact logo' className='popover' />
